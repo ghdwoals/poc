3fb076d5c0e11cef980d191d80d066c825e5018d
ghdwoals <mirrorshades@protonmail.com>
2016-07-30 07:27:12 -0400

    On branch master  Your branch is up-to-date with 'origin/master'. 	modified:   exercise02.c

--- ch02/exercise02.c
+++ ch02/exercise02.c
@@ -1,4 +1,19 @@
+/*
+** Write a program that will read C source code from the standard input and
+**  ensure that the braces are paired correctly. Note: you need not worry about
+**  braces that appear within comments, string literals, or character constants.
+*/
+
 #include <stdio.h>
+
+/*
+** Random brace here: {
+*/
+
+// Another: }
+
+#define BRACE   {
+#define BRACE2  '{'
 
 int main(void) {
     int in_comment = 0;
@@ -10,22 +25,41 @@ int main(void) {
     while ((ch = getchar()) != EOF) {
         if (ch == '/') {
             ch = getchar();
-            if (ch == '*')
+            if (ch == '*' && in_comment == 0)
                 in_comment = 1;
+            if (ch == '/') {
+                while ((ch = getchar()) != '\n')
+                    ;
+            }
         } else if (ch == '*') {
             ch = getchar();
-            if (ch == '/')
+            if (ch == '/' && in_comment)
                 in_comment = 0;
         }
 
+        if (ch == '\"' && in_comment)
+            in_comment = 0;
+        else if (ch == '\"' && in_comment == 0)
+            in_comment = 1;
+
+        if (ch == '\'') {
+            ch = getchar();
+            if (ch == '{' || ch == '}')
+                continue;
+        }
+
+        if (ch == '#')
+            while ((ch = getchar()) != '\n')
+                continue;
+
         if (in_comment)
             continue;
-
+        
         if (ch == '{') {
             braces++;
             open_braces++;
         }
-        if (ch == '}') {
+        else if (ch == '}') {
             braces--;
             close_braces++;
         }
